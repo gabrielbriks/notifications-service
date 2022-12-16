dae0cdf777a1cbf9e166454f13a5bdc96f6de15b
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2022-12-16 19:20:28 -0300

    Cria entidade notifications e ValueObject Content
    Aplica ajuste na organização dos arquivos, criado a entidade juntamente
    com os getters e setters. Foi incluído também uma class Content para
    utilizarmos como Value Object do atributo content da nossa entidade.

---
+++ src/application/entities/notification/notification.ts
@@ -0,0 +1,53 @@
+import { Content } from './content';
+
+interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(value: string) {
+    this.props.recipientId = value;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(value: Content) {
+    this.props.content = value;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(value: string) {
+    this.props.category = value;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(value: Date | null | undefined) {
+    this.props.readAt = value;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createAt(): Date {
+    return this.props.createAt;
+  }
+}
